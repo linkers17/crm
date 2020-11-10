ee728643b04976022e7aa59660c699beed8c4d2f
linkers17 <romeo-gry@mail.ru>
2020-11-10 18:34:22 +0700

    Сделал роуты для получения одной и всех папок

--- backend/controllers/folders.js
+++ backend/controllers/folders.js
@@ -4,6 +4,22 @@ const errorHandler = require('../utils/errorHandler');
 module.exports.getFolders = async (req, res) => {
     try {
 
+        const folders = await Folders.find({});
+
+        if (!folders) {
+            return res.status(404).json({errors: 'Ниодной папки не найдено'});
+        } else {
+
+            let makeTree = array => array.filter(item => {
+                item._doc.childList = array.filter(i => i.parentId == item._id);
+                return item.parentId === '';
+            });
+
+            const newFolders = makeTree(folders);
+
+            res.json(newFolders);
+        }
+
     } catch (err) {
         return errorHandler(res, err);
     }
@@ -11,6 +27,34 @@ module.exports.getFolders = async (req, res) => {
 
 module.exports.getFolderById = async (req, res) => {
     try {
+
+        const folder = await Folders.findById(req.params.id);
+
+        if (!folder) {
+            return res.status(404).json({errors: 'Такой папки не существует или она была удалена'});
+        } else {
+
+            // Ищем родителя
+            let parent = null;
+            if (folder.parentId) {
+                parent = await Folders.findById(folder.parentId, 'id name');
+            }
+
+            // Ищем потомков
+            const children = await Folders.find({parentId: folder._id}, 'id name');
+
+            // Ищем документы в папке
+            //const documents = await documents.find({folderId: folder._id});
+
+            return res.status(200).json({
+                folder: {
+                    folder,
+                    parent,
+                    children,
+                    //documents
+                }
+            });
+        }
 
     } catch (err) {
         return errorHandler(res, err);
@@ -22,6 +66,9 @@ module.exports.getFolderById = async (req, res) => {
 // 5fa97106753c882988e37caa -> Папка 3 (1)
 // 5fa9715d753c882988e37cab -> Папка 2 (1)
 // 5fa97197753c882988e37cac -> Папка 3
+// 5faa1fdaa4f9a62fbcd25ca4 -> Папка 4 (2)
+// 5faa2016a4f9a62fbcd25ca5 -> Папка 5 (3)
+
 module.exports.createFolder = async (req, res) => {
     try {
 
