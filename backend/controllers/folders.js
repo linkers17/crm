030cd75d34a48ecc0a58c5da36ed6d7493a208ce
linkers17 <romeo-gry@mail.ru>
2020-11-10 22:11:31 +0700

    Добавил роут обновления папки

--- backend/controllers/folders.js
+++ backend/controllers/folders.js
@@ -63,11 +63,12 @@ module.exports.getFolderById = async (req, res) => {
 
 // 5fa9705c7dc0812c90f421dc -> Папка 1
 // 5fa970ca753c882988e37ca9 -> Папка 2
-// 5fa97106753c882988e37caa -> Папка 3 (1)
+// 5fa97106753c882988e37caa -> Папка 8 (1)
 // 5fa9715d753c882988e37cab -> Папка 2 (1)
-// 5fa97197753c882988e37cac -> Папка 3
+// 5fa97197753c882988e37cac -> Папка 6
 // 5faa1fdaa4f9a62fbcd25ca4 -> Папка 4 (2)
 // 5faa2016a4f9a62fbcd25ca5 -> Папка 5 (3)
+// 5faa76aabefa2c2eccafbc80 -> Папка 7 (4)
 
 module.exports.createFolder = async (req, res) => {
     try {
@@ -98,6 +99,33 @@ module.exports.createFolder = async (req, res) => {
 
 module.exports.updateFolder = async (req, res) => {
     try {
+
+        const candidate = await Folders.findOne({
+            name: req.body.name,
+            parentId: req.body.parent
+        });
+
+        if (req.body.name.trim() === '') {
+            return res.status(409).json({errors: 'Имя папки не может быть пустым'});
+        }
+
+        if (candidate) {
+            return res.status(409).json({errors: 'Папка с таким именем уже существует.'});
+        } else {
+            const folder = await Folders.findOneAndUpdate(
+                {_id: req.params.id},
+                {$set: {
+                    name: req.body.name,
+                    description: req.body.description,
+                    updatedById: req.user.id,
+                    updatedByLogin: req.user.login,
+                    parentId: req.body.parent
+                }},
+                {new: true}
+            );
+
+            res.status(200).json(folder);
+        }
 
     } catch (err) {
         return errorHandler(res, err);
