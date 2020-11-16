67af52dba070bd247f988a4912533a1c9623a3e4
linkers17 <romeo-gry@mail.ru>
2020-11-16 23:01:32 +0700

    Изменил роут редактирования папки

--- backend/controllers/folders.js
+++ backend/controllers/folders.js
@@ -61,15 +61,6 @@ module.exports.getFolderById = async (req, res) => {
     }
 }
 
-// 5fa9705c7dc0812c90f421dc -> Папка 1
-// 5fa970ca753c882988e37ca9 -> Папка 2
-// 5fa97106753c882988e37caa -> Папка 8 (1)
-// 5fa9715d753c882988e37cab -> Папка 2 (1)
-// 5fa97197753c882988e37cac -> Папка 6
-// 5faa1fdaa4f9a62fbcd25ca4 -> Папка 4 (2)
-// 5faa2016a4f9a62fbcd25ca5 -> Папка 5 (3)
-// 5faa76aabefa2c2eccafbc80 -> Папка 7 (4)
-
 module.exports.createFolder = async (req, res) => {
     try {
 
@@ -114,9 +105,10 @@ module.exports.createFolder = async (req, res) => {
 // 5faac7c400303a3e285679d1 -> Тест 3-1
 // 5faac7e400303a3e285679d2 -> Тест 4-2
 // 5faac7f900303a3e285679d3 -> Тест 5-1
-// 5faac81000303a3e285679d4 -> Тест 6-3
+// 5faac81000303a3e285679d4 -> Тест 6
 // 5faac83500303a3e285679d5 -> Тест 7-6
-// 5faac883dad32b2090390af3 -> Тест 8
+// 5faac883dad32b2090390af3 -> Тест 8-7
+// 5fb219d4d3272e3eb813fca7 -> Тест 9-6
 
 
 module.exports.updateFolder = async (req, res) => {
@@ -127,24 +119,53 @@ module.exports.updateFolder = async (req, res) => {
             parentId: req.body.parent
         });
 
+        const folder = await Folders.findById(req.params.id);
+
+        // Ограничение прав редактирования
+        if (req.user.role !== 'director' && req.user.role !== 'admin' &&
+            folder.createdById != req.user.id) {
+            
+            return res.status(409).json({errors: 'Вы не можете редактировать эту папку'});
+        }
+
+        // Если поле "название папки" пустое
         if (req.body.name.trim() === '') {
             return res.status(409).json({errors: 'Имя папки не может быть пустым'});
         }
 
+        // Если имя папки уже занято
         if (candidate) {
             return res.status(409).json({errors: 'Папка с таким именем уже существует.'});
         } else {
+
+            // Находим родителя, если он указан
+
+            const parent = req.body.parent !== '' ? await Folders.findOne({_id: req.body.parent}) : null;
+
+            const ids = parent ? [...parent.parentIds, req.body.parent] : [];
+
+            const odlParentId = await Folders.findById({_id: req.params.id}, 'parentId parentIds');
+
             const folder = await Folders.findOneAndUpdate(
                 {_id: req.params.id},
-                {$set: {
-                    name: req.body.name,
-                    description: req.body.description,
-                    updatedById: req.user.id,
-                    updatedByLogin: req.user.login,
-                    parentId: req.body.parent
-                }},
+                {
+                    $set: {
+                        name: req.body.name,
+                        description: req.body.description,
+                        updatedById: req.user.id,
+                        updatedByLogin: req.user.login,
+                        parentId: req.body.parent,
+                        parentIds: ids
+                    }
+                },
                 {new: true}
             );
+
+            // Изменяем зависимости у детей  
+            const children = await Folders.find({parentIds: {$in: [req.params.id]}});
+            children.map(async (child) => {
+                await Folders.updateOne({_id: child._id}, {$set: {parentIds: [...folder.parentIds, ...child.parentIds.filter(id => !odlParentId.parentIds.includes(id)/*id != odlParentId.parentId*/)]}})
+            });
 
             res.status(200).json(folder);
         }
