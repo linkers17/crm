15a869364d70eec2f94e85963bfcba5af9f9d00b
linkers17 <romeo-gry@mail.ru>
2020-11-18 12:03:05 +0700

    Добавил модель документа

---
+++ backend/models/Documents.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const documentsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: [
+            'active',   // Действующий
+            'draft',    // Черновик
+            'expired',  // Просрочен
+            'canceled'  // Отменен
+        ],
+        default: 'active'
+    },
+    type: {
+        type: String,
+        enum: [
+            'agreement',    // Договор
+            'contract',     // Контракт
+            'pattern',      // Шаблон
+            'report'        // Отчёт
+        ],
+        default: ''
+    },
+    createdById: {
+        ref: 'users',
+        type: Schema.Types.ObjectId
+    },
+    createdByLogin: String,
+    updatedById: {
+        ref: 'users',
+        type: Schema.Types.ObjectId,
+        default: null
+    },
+    updatedByLogin: {
+        type: String,
+        default: ''
+    },
+    assignedUserId: {
+        ref: 'users',
+        type: Schema.Types.ObjectId,
+        default: null
+    },
+    folderId: {
+        ref: 'folders',
+        type: Schema.Types.ObjectId,
+        default: null
+    },
+    description: String,
+    filePath: String
+
+}, {timestamps: true});
+
+module.exports = mongoose.model('documents', documentsSchema);
