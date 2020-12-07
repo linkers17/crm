c019f5adc6d7ac2e124f599153cf3bef0b342f75
linkers17 <romeo-gry@mail.ru>
2020-12-07 19:32:49 +0700

    Добавил роут просмотра всех услуг

--- backend/controllers/services.js
+++ backend/controllers/services.js
@@ -4,7 +4,34 @@ const errorHandler = require('../utils/errorHandler');
 module.exports.getServices = async (req, res) => {
     try {
 
-        
+        const query = {};
+
+        if (req.query.title) {
+            query.title = req.query.title;
+        }
+
+        if (req.query.minAmount) {
+            query.amount = {
+                $gte: req.query.minAmount
+            };
+        }
+
+        if (req.query.maxAmount) {
+            if (!query.amount) {
+                query.amount = {}
+            }
+
+            query.amount['$lte'] = req.query.maxAmount;
+        }
+
+        if (req.query.title) {
+            query.title = req.query.title;
+        }
+
+        const services = await Services.find(query);
+        const servicesCount = await Services.countDocuments(query);
+
+        res.status(200).json({services, servicesCount});
 
     } catch (err) {
         return errorHandler(res, err);
@@ -29,6 +56,12 @@ module.exports.createService = async (req, res) => {
         if (candidate) {
             return res.status(409).json({errors: 'Услуга с таким названием уже существует'});
         } else {
+
+            // Проверяем стоимость на отрицательное значение
+            if (+req.body.amount < 0) {
+                return res.status(409).json({errors: 'Стоимость не может быть меньше 0.'});
+            }
+
             const service = await new Services({
                 title: req.body.title,
                 amount: +req.body.amount,
