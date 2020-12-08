a79563c43d362e39a862d88743cb39e27e426db0
linkers17 <romeo-gry@mail.ru>
2020-12-08 23:18:39 +0700

    Добавил роут создания задачи

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -28,7 +28,36 @@ module.exports.createTask = async (req, res) => {
 
     try {
 
+        const parentId = req.query.parentId ? req.query.parentId : null;
         
+        // Если создается уже завершенная задача
+        const dateEnd = req.body.status === 'completed' ? Date.now() : null;
+
+        // Проверка на выбор документа
+        if (req.body.documentIds) {
+            for (let i = 0; i < req.body.documentIds.length; i++) { 
+                if (req.body.documentIds[i].trim() === '') {
+                    return res.status(409).json({errors: 'Документ должен быть выбран'});
+                }
+            }
+        }
+
+        const task = await new Tasks({
+            title: req.body.title,
+            description: req.body.description,
+            deadline: req.body.deadline,
+            priority: req.body.priority,
+            status: req.body.status,
+            parentId,
+            dateEnd,
+            documentIds: req.body.documentIds,
+            assignedUserId: req.body.assignedUserId ? req.body.assignedUserId : req.user.id, 
+            createdById: req.user.id,
+            createdByLogin: req.user.login
+
+        }).save();
+
+        res.status(201).json(task);
         
     } catch (err) {
         return errorHandler(res, err);
