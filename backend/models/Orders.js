515f599f567a009660bbe54b93cd588bc977fa14
linkers17 <romeo-gry@mail.ru>
2020-12-16 15:57:21 +0700

    Создал модель заказов

---
+++ backend/models/Orders.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ordersSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    stage: {
+        type: String,
+        enum: [
+            'prospecting',
+            'offer',
+            'negotiation',
+            'closed won',
+            'closed loose'
+        ],
+        default: 'prospecting'
+    },
+    servicesList: [
+        {
+            title: String,
+            quantity: Number,
+            amount: Number
+        }
+    ],
+    createdById: {
+        ref: 'users',
+        type: Schema.Types.ObjectId
+    },
+    createdByLogin: String,
+    updatedById: {
+        ref: 'users',
+        type: Schema.Types.ObjectId,
+        default: null
+    },
+    updatedByLogin: String,
+    dateEnd: {
+        type: Date,
+        default: null
+    },
+    assignedUserId: {
+        ref: 'users',
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    documentIds: {
+        ref: 'documents',
+        type: [Schema.Types.ObjectId]
+    },
+    customerId: {
+        ref: 'customers',
+        type: Schema.Types.ObjectId
+    },
+    companyId: {
+        ref: 'companies',
+        type: Schema.Types.ObjectId
+    },
+    amount: Number,
+    description: String
+}, {timestamps: true});
+
+module.exports = mongoose.model('orders', ordersSchema);
