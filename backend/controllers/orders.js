eec2bc69af8c3115020711dc0607826e5ba27e96
linkers17 <romeo-gry@mail.ru>
2020-12-17 19:45:25 +0700

    Добавил роут получения всех заказов

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,10 +1,67 @@
 const Orders = require('../models/Orders');
 const errorHandler = require('../utils/errorHandler');
+const mongoose = require('mongoose');
 
 module.exports.getOrders = async (req, res) => {
     try {
 
-        
+        // По-умолчанию отображаются заказы, в которых пользователь ответственный
+        const query = {};
+        if (req.user.role === 'manager') {
+            query.assignedUserId = mongoose.Types.ObjectId(req.user.id);
+        }
+
+        // Фильтр стадий заказа
+        if (
+            req.query.prospecting ||
+            req.query.offer ||
+            req.query.negotiation ||
+            req.query.closedWon ||
+            req.query.closedLoose
+        ) {
+            const stage = {$in: []};
+            req.query.prospecting ? stage.$in.push('prospecting') : false;
+            req.query.offer ? stage.$in.push('offer') : false;
+            req.query.negotiation ? stage.$in.push('negotiation') : false;
+            req.query.closedWon ? stage.$in.push('closed won') : false;
+            req.query.closedLoose ? stage.$in.push('closed loose') : false;
+            query.stage = stage;
+        }
+
+        // Фильтр стоимости заказа
+        if (req.query.minAmount) {
+            query.amount = {
+                $gte: +req.query.minAmount
+            }
+        }
+
+        if (req.query.maxAmount) {
+            if (!query.amount) {
+                query.amount = {};
+            }
+            query.amount['$lte'] = +req.query.maxAmount;
+        }
+
+        // Фильтр даты завершения заказа
+        if (req.query.startDateEnd) {
+            query.dateEnd = {
+                $gte: req.query.startDateEnd
+            }
+        }
+
+        if (req.query.endDateEnd) {
+            if (!query.dateEnd) {
+                query.dateEnd = {};
+            }
+            query.dateEnd['$lte'] = req.query.endDateEnd;
+        }
+
+        console.log('query', query);
+
+        const orders = await Orders.find(query, 'stage title dateEnd amount createdByLogin createdById createdAt');
+        const count = await Orders.countDocuments(query);
+
+        res.status(200).json({orders, count});
 
     } catch (err) {
         return errorHandler(res, err);
