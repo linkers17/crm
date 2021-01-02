2cacf17c6acb7c3d44e38eee1878148523b35124
linkers17 <romeo-gry@mail.ru>
2021-01-02 18:09:07 +0700

    Добавил angular material

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://localhost:7777/api',
+  API_UPLOADS: 'http://localhost:7777/uploads'
 };
 
 /*
