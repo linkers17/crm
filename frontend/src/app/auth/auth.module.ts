678dc87089a24f57b7f0eccd97931144360bb1db
linkers17 <romeo-gry@mail.ru>
2021-01-04 15:00:54 +0700

    Верстка формы регистрации (еще не закончена)

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import {MatInputModule} from "@angular/material/input";
+import {ErrorStateMatcher, ShowOnDirtyErrorStateMatcher} from "@angular/material/core";
+import {NgxMatFileInputModule} from "@angular-material-components/file-input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    NgxMatFileInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
   ]
 })
 export class AuthModule { }
--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  accept = '.png, .jpg, .jpeg';
+  hide = true;
+  phones = [{id: 1, input: ''}];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  addPhone(): void {
+    this.phones = this.phones.filter(phoneInput => phoneInput.input.trim() !== '');
+    this.phones.push({id: this.phones[this.phones.length - 1].id + 1, input: ''});
+  }
+
+  removePhone(id: number): void {
+    this.phones = this.phones.filter(phoneInput => phoneInput.id !== id);
   }
 
 }
