596937f1c5b1fbcc31ea809330c2633213d10925
linkers17 <romeo-gry@mail.ru>
2021-01-06 17:37:21 +0700

    Сверстал форму логина

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup;
+  hide = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm(): void {
+    this.form = new FormGroup({
+      login: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/^[a-zA-z0-9]+$/m)
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]+$/m)
+      ])
+    });
+  }
+
+  onSubmit(): void {
+
+  }
 }
