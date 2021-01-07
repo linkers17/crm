c27d289a0a77d3223210fd32484c1ce35d11b5d3
linkers17 <romeo-gry@mail.ru>
2021-01-07 15:00:08 +0700

    Добавил интерфейсы для currentUser, отправки данных на регистрацию и файла

---
+++ frontend/src/app/auth/types/registerRequest.interface.ts
@@ -0,0 +1,14 @@
+import {FileInterface} from "../../shared/types/file.interface";
+
+export interface RegisterRequestInterface {
+  address: string,
+  birthday: string,
+  email: string,
+  login: string,
+  name: string,
+  password: string,
+  patronym: string,
+  phones: string[],
+  surname: string,
+  userImg: null | FileInterface
+}
---
+++ frontend/src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,6 @@
+export interface CurrentUserInterface {
+  login: string,
+  role: string,
+  token: string,
+  userImg: string
+}
